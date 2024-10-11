7932c5e1aa11ae0802c34f1a6336a5e065e7fc3d
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2024-10-11 13:15:05 +0800

    Initialize

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chat', [ChatController::class, 'index']);
